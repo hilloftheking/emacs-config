e440cd9943e75d5592582614e46f2b4f65217cfd
hilloftheking <91853499+hilloftheking@users.noreply.github.com>
2023-02-11 20:47:14 -0500

    Update init.el

--- init.el
+++ init.el
@@ -1,6 +1,5 @@
 ;; Packages
 (require 'package)
-(setq package-enable-at-startup nil)
 (add-to-list 'package-archives
 	     '("melpa" . "https://melpa.org/packages/"))
 (package-initialize)
@@ -23,6 +22,8 @@
   (add-hook 'c-mode-hook 'lsp-mode)
   (add-hook 'cpp-mode-hook 'lsp-mode)
   (add-hook 'objc-mode-hook 'lsp-mode))
+(use-package magit)
+(use-package magithub)
 (use-package sly
   :config
   (setq inferior-lisp-program "sbcl"))
@@ -33,13 +34,25 @@
   :config
   (add-hook 'c-mode-hook 'clang-format+-mode)
   (add-hook 'cpp-mode-hook 'clang-format+-mode))
+(use-package rainbow-mode
+  :config
+  (add-hook 'prog-mode-hook 'rainbow-mode))
 (use-package gruvbox-theme)
+
+;; Make the buffer list better
+(bind-key "C-x C-b" #'electric-buffer-list)
 
 ;; Always display line numbers
 (add-hook 'prog-mode-hook 'display-line-numbers-mode)
 
+;; Side buttons on mouse to move between buffers
+(bind-key "<mouse-9>" 'next-buffer)
+(bind-key "<mouse-8>" 'previous-buffer)
+
 ;; Extension for common lisp (.cl)
 (add-to-list 'auto-mode-alist '("\\.cl\\'" . common-lisp-mode))
+;; .stumpwmrc file should open in common-lisp-mode
+(add-to-list 'auto-mode-alist '("/.stumpwmrc\\'" . common-lisp-mode))
 
 ;; Frame Configuration
 (menu-bar-mode -1)
@@ -57,10 +70,10 @@
    '("d80952c58cf1b06d936b1392c38230b74ae1a2a6729594770762dc0779ac66b7" default))
  '(ispell-dictionary nil)
  '(package-selected-packages
-   '(clang-format+ clang-format lsp-ui lsp-mode sly flycheck gruvbox-theme which-key use-package)))
+   '(magithub magit rainbow-mode clang-format+ clang-format lsp-ui lsp-mode sly flycheck gruvbox-theme which-key use-package company)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(default ((t (:inherit nil :extend nil :stipple nil :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 98 :width normal :foundry "ADBO" :family "Monospace")))))
+ '(default ((t (:inherit nil :extend nil :stipple nil :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 112 :width normal :foundry "ADBO" :family "Terminus")))))
