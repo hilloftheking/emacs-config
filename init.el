c070a0ccb249c9679ec32cb2e93ce1ad71e0adf7
hilloftheking <91853499+hilloftheking@users.noreply.github.com>
2023-03-11 00:54:52 -0500

    Fix hooks

--- init.el
+++ init.el
@@ -12,31 +12,30 @@
 (setq use-package-always-ensure t)
 
 (use-package which-key
-  :config
+  :init
   (which-key-mode))
 (use-package company
-  :config
+  :init
   (global-company-mode))
 (use-package lsp-mode
-  :config
-  (add-hook 'c-mode-hook 'lsp-mode)
-  (add-hook 'cpp-mode-hook 'lsp-mode)
-  (add-hook 'objc-mode-hook 'lsp-mode))
+  :hook
+  (c-mode-common . lsp))
+(use-package gdscript-mode)
 (use-package magit)
 (use-package magithub)
 (use-package sly
-  :config
+  :init
   (setq inferior-lisp-program "sbcl"))
 (use-package flycheck
-  :config
+  :init
   (global-flycheck-mode))
 (use-package clang-format+
-  :config
-  (add-hook 'c-mode-hook 'clang-format+-mode)
-  (add-hook 'cpp-mode-hook 'clang-format+-mode))
+  :hook
+  (c-mode . clang-format+-mode)
+  (cpp-mode . clang-format+-mode))
 (use-package rainbow-mode
-  :config
-  (add-hook 'prog-mode-hook 'rainbow-mode))
+  :hook
+  (prog-mode . rainbow-mode))
 (use-package gruvbox-theme)
 
 ;; Make the buffer list better
@@ -75,9 +74,12 @@
    '("d80952c58cf1b06d936b1392c38230b74ae1a2a6729594770762dc0779ac66b7" default))
  '(ispell-dictionary nil)
  '(package-selected-packages
-   '(magithub magit rainbow-mode clang-format+ clang-format lsp-ui lsp-mode sly flycheck gruvbox-theme which-key use-package company)))
+   '(gdscript-mode magithub magit rainbow-mode clang-format+ clang-format lsp-ui lsp-mode sly flycheck gruvbox-theme which-key use-package company)))
 
 (custom-set-faces
- ;; Terminus on desktop, default for monospace on laptop
- '(default ((t (:inherit nil :extend nil :stipple nil :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 112 :width normal :foundry "*"
-			 :family (if (= (getenv "HOSTNAME") "machine") "Terminus" "Monospace"))))))
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(default ((t (:inherit nil :extend nil :stipple nil :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 112 :width normal :foundry "*" :family (if (= (getenv "HOSTNAME") "machine") "Terminus" "Monospace"))))))
+(put 'scroll-left 'disabled nil)
