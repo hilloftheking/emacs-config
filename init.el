7909f8b4d1e2e7c671ac9399c74bca3c7dde1b03
hilloftheking <91853499+hilloftheking@users.noreply.github.com>
2025-11-22 02:39:03 -0500

    yet another update

--- init.el
+++ init.el
@@ -1,15 +1,70 @@
-(context-menu-mode 1)
-(tool-bar-mode -1)
+(require 'package)
+(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
+(package-initialize)
 
-(setq make-backup-files nil)
-(setq gdb-many-windows t)
-(setq dired-listing-switches (concat dired-listing-switches "h"))
-(setq read-file-name-completion-ignore-case t
-      read-buffer-completion-ignore-case t
-      completion-ignore-case t)
+(require 'use-package)
+(require 'use-package-ensure)
+(setq use-package-always-ensure t)
 
-(column-number-mode 1)
-(add-hook 'prog-mode-hook 'display-line-numbers-mode)
+;;;; ** LSP and Completion **
+
+(use-package eglot
+  :config
+  (setq eglot-ignored-server-capabilities '(:inlayHintProvider)))
+
+(use-package vertico
+  :config
+  (vertico-mode))
+(use-package orderless
+  :custom
+  (completion-styles '(orderless basic partial-completion))
+  (completion-category-defaults nil) ;; Disable defaults, use our settings
+  (completion-pcm-leading-wildcard t)) ;; Emacs 31: partial-completion behaves like substring
+(use-package corfu
+  :custom
+  (corfu-auto t)
+  (corfu-auto-delay 0.1)
+  (corfu-auto-prefix 2)
+  (corfu-auto-trigger ".:")
+  :config
+  (global-corfu-mode)
+  ;; Don't use enter key for completions
+  (keymap-unset corfu-map "RET"))
+
+;;;; ** Debugging **
+
+(use-package dape
+  :custom
+  ;; Set breakpoints with mouse
+  (dape-breakpoint-global-mode +1)
+  ;; Pulse source line (performance hit)
+  (add-hook 'dape-display-source-hook #'pulse-momentary-highlight-one-line))
+
+;;;; ** Languages **
+
+(use-package markdown-mode)
+(use-package glsl-mode)
+(use-package lua-mode)
+(use-package rust-mode)
+
+(add-to-list 'eglot-server-programs
+	     '(csharp-mode . ("csharp-ls")))
+
+;;;; ** Formatting **
+
+(use-package clang-format-lite
+  :hook
+  ((c-mode . clang-format-lite-save-hook) (c++-mode . clang-format-lite-save-hook)))
+(use-package editorconfig
+  :config
+  (editorconfig-mode 1))
+
+;;;; ** Keybinds **
+
+(use-package meow) ;; Setup for this is further down
+(use-package which-key
+  :config
+  (which-key-mode 1))
 
 (keymap-global-set "C-x b" 'electric-buffer-list)
 (keymap-global-set "C-x C-b" 'switch-to-buffer)
@@ -19,54 +74,19 @@
 (with-eval-after-load "dired"
   (define-key dired-mode-map [mouse-2] 'dired-mouse-find-file))
 
-(setq mouse-wheel-progressive-speed nil)
-(setcar mouse-wheel-scroll-amount 5)
+;;;; ** Miscellaneous **
 
-(load "~/.emacs.d/exotica-theme.el")
-(load-theme 'exotica t)
-
-(require 'package)
-(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
-(package-initialize)
-
-(require 'use-package)
-(require 'use-package-ensure)
-(setq use-package-always-ensure t)
-
-(use-package eglot
-  :config
-  (setq eglot-ignored-server-capabilities '(:inlayHintProvider)))
-(use-package vertico
-  :config
-  (vertico-mode))
-(use-package orderless
-  :custom
-  (completion-styles '(orderless basic))
-  (completion-category-overrides '((file (styles partial-completion))))
-  (completion-category-defaults nil) ;; Disable defaults, use our settings
-  (completion-pcm-leading-wildcard t)) ;; Emacs 31: partial-completion behaves like substring
 (use-package magit)
-(use-package corfu
-  :custom
-  (corfu-auto t)
-  :config
-  (global-corfu-mode)
-  ;; Don't use enter key for completions
-  (keymap-unset corfu-map "RET"))
-(use-package meow)
-(use-package which-key
-  :config
-  (which-key-mode 1))
-(use-package glsl-mode)
-(use-package lua-mode)
-(use-package clang-format-lite
-  :hook
-  ((c-mode . clang-format-lite-save-hook) (c++-mode . clang-format-lite-save-hook)))
-(use-package editorconfig
-  :config
-  (editorconfig-mode 1))
 (use-package eat
   :hook ((eshell-load . eat-eshell-mode) (eshell-load . eat-eshell-visual-command-mode)))
+
+;;;; ** Theme **
+
+(use-package color-theme-sanityinc-tomorrow
+  :config
+  (load-theme 'sanityinc-tomorrow-night t))
+
+;;;; ** Meow Setup **
 
 (defun meow-setup ()
   (setq meow-cheatsheet-layout meow-cheatsheet-layout-qwerty)
@@ -150,8 +170,44 @@
    '("Y" . meow-sync-grab)
    '("z" . meow-pop-selection)
    '("'" . repeat)
-   '("<escape>" . ignore)))
+   '("<escape>" . ignore)
+   '("?" . eldoc-print-current-symbol-info)))
 
 (meow-setup)
 (meow-global-mode 1)
 
+;;;; ** Tweaks **
+
+(context-menu-mode 1)
+(tool-bar-mode -1)
+
+(setq make-backup-files nil)
+(setq gdb-many-windows t)
+(setq dired-listing-switches (concat dired-listing-switches "h"))
+(setq read-file-name-completion-ignore-case t
+      read-buffer-completion-ignore-case t
+      completion-ignore-case t)
+
+(column-number-mode 1)
+(add-hook 'prog-mode-hook 'display-line-numbers-mode)
+
+(setq mouse-wheel-progressive-speed nil)
+(setcar mouse-wheel-scroll-amount 5)
+
+;;;; ** Custom **
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   '(clang-format-lite color-theme-sanityinc-tomorrow corfu dape eat
+		       glsl-mode lua-mode magit markdown-mode meow
+		       orderless rust-mode vertico)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
